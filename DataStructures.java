79ab990fae4add7a5fbca6fc47c3b07684900997
KAGENTE <kaique.fq.1@gmail.com>
2024-11-19 22:51:25 -0300

    just commented the code to make sure i am understanding it

--- DataStructures.java
+++ DataStructures.java
@@ -36,14 +36,17 @@ class DataStructures{
             }
             swap(list, lowestIndex, i); //now working with the indexes instead of the numbers it self we can use the new swap method
         }
-        
+
     }
+
+
+              //Old code
 /*             int temp = list.get(i);//swap the values of the current index to the lowest number encountered on the remaining array
             list.set(i, lowest);//cant be switched to swap method since we are not swiching indexes but abstraction of lowest number
             list.set(j, temp); */
            
     
-
+            //Even more old code
         /* for(int i = 0;i< list.size();i++){           Code i made  in the fist contact with DSA and selection sort
             int menor = list.get(i);                    had some troubble to understand but tried to do by myself knowing it wont be as good as it coud be
             int indexJ = i;                         
@@ -59,63 +62,65 @@ class DataStructures{
             list.set(indexJ, temp);
        } */
     
-    
     public static int bynarySearch(ArrayList<Integer> list, int target){
-        int start = 0;
-        int end = list.size()-1;
+        int start = 0; //first index of the array
+        int end = list.size()-1;//size is -1 because of index that starts at 0
         int half = 0;
-        while(list.get(half) != target){
-            half = (end + start)/2;
-            if(target > list.get(half)){
-                start = half+1;
-            }else if (target < list.get(half)){
-                end = half-1;
+            while(list.get(half) != target){//need to find the number in the half index
+                half = (end+start)/2;// getting to the half of the string
+                if(list.get(half) < target){
+                    start = half+1;//if target is bigger then you bring the start variable to the number at front of the old half variable
+                }
+                else if(half > target){
+                    end = half-1;//else if target is less than targer then you bring the end variable to the index before
+                }
+                
             }
-        }return half+1;
-
-    }
+            return half+1;//return plus one because array start on 0
+        }
+    
 
     public static void bubbleSort(ArrayList<Integer> list){
-            for(int i= 0;i < list.size();i++){
-                for(int j=0;j< list.size()-i-1;j++){
-                    if(list.get(j)>list.get(j+1)){
-                        swap(list, j, j+1);
-                    }
+        for(int i= 0;i < list.size();i++){//first loop to go through the indexes of string
+            for(int j=0;j< list.size()-i-1;j++){//second loop to actualy swap the numbers
+                if(list.get(j)>list.get(j+1)){
+                    swap(list, j, j+1);//here you use the index in front of j instead a value from other loop as the 2 loops will go 
+                                       //trough the array 1 time each
                 }
             }
+        }
     }
 
-    public static ArrayList<Integer> insertionSort(ArrayList<Integer> list){
+    public static void insertionSort(ArrayList<Integer> list){
         
-        for(int i=1; i < list.size();i++){
-            int temp = list.get(i);
-            int j = i-1;
-            while(j>=0 && list.get(j) > temp){
-                list.set(j+1, list.get(j));
-                j--;
-            }  
-            list.set(j+1, temp);
+        for(int i=1; i < list.size();i++){//loop through the array but starts at 1 because j will be 1 less than i
+            int temp = list.get(i);//hold the value of i that will be changed in the future
+            int j = i-1;//1 less than i, will start in index 0
+                while(j>=0 && list.get(j) > temp){//test j value with i value that is temp variable
+                    list.set(j+1, list.get(j));//set the value of the index in front of j (i) with the j index value
+                    j--;//at this point we are bringing all the smallers values to the start so j-- will keep testing values behind the i variable
+                }  
+        list.set(j+1, temp);//set the value of the number in front of j to the old value of i
         }
-        return list;
     }
 
     public static void quickSort(ArrayList<Integer> list,int low,int high){
-        if(low>=high){
+        if(low>=high){//end the loop if the pointers meet
             return;
         }
-       int pivot =list.get(high);
+       int pivot =list.get(high);//the number we need to find the correct position to then divide the array and keep looping till its sorted
        int left = low;
        int right = high;
 
-       while(left < right){
+       while(left < right){//to keep looping till the indexes meet
 
-        while (list.get(left) <= pivot && left < right){
+        while (list.get(left) <= pivot && left < right){//keep adding up to left, highering the index will break if find a number less than the pivot
            left++; 
         }
-        while(list.get(right) >= pivot && left < right){
+        while(list.get(right) >= pivot && left < right){//keep subtracting, lowering the index, will break if find a number higher than the pivot
             right--;
         }
-        swap(list, left, right);
+        swap(list, left, right);//then when the 2 loops break will swap the numbers
        }
        swap(list , left , high);
        quickSort(list, low, left-1);
